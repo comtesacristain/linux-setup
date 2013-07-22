4105432113bf1c4dcdebdabc20ffadd59b68b214
Michael Sexton <mick.sexton@gmail.com>
2013-07-22 23:53:29 +1000

    Added git config parameters, maybe put it in protected file

--- git.sh
+++ git.sh
@@ -1,6 +1,8 @@
 #!
 cd ~/Public
 sudo apt-get install git
+git config --global user.email "mick.sexton@gmail.com"
+git config --global user.name "Michael Sexton"
 git clone https://github.com/comtesacristain/linux-setup.git
 cd linux-setup
 ./install.sh
