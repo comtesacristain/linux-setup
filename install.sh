00b6a39be34cd872b5eee11cbf6484852325f53d
Michael Sexton <mick.sexton@gmail.com>
2013-07-24 15:26:14 +1000

    Added simple mode for push in git

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ cd ~/Public
 sudo apt-get install git
 git config --global user.email "mick.sexton@gmail.com"
 git config --global user.name "Michael Sexton"
+git config --global push.default simple
 git clone https://github.com/comtesacristain/linux-setup.git
 cd linux-setup
 
