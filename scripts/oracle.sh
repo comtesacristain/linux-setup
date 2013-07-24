fe4705210fff0f4c1b9f74216fa977613d768b22
Michael Sexton <mick.sexton@gmail.com>
2013-07-24 21:46:16 +1000

    Added setup path variable for Oracle install

--- scripts/oracle.sh
+++ scripts/oracle.sh
@@ -4,4 +4,4 @@ wget http://oss.oracle.com/el4/RPM-GPG-KEY-oracle  -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install oracle-xe-universal
 sudo /etc/init.d/oracle-xe configure
-cat oracle/.oraclerc >> ~/.bashrc
+cat $LSPATH/oracle/.oraclerc >> ~/.bashrc
