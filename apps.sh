6cdf409504019a9b50e230fe0479535c726899e0
Michael Sexton <mick.sexton@gmail.com>
2013-07-24 15:15:15 +1000

    One install script that installs git and clones setup repository then executes remaining scripts

--- apps.sh
+++ apps.sh
@@ -1,4 +1,4 @@
 #!
 sudo apt-get install chromium-browser
 sudo apt-get install curl
-
+sudo apt-get install vim
--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
-sudo apt-get upgrade
-sudo apt-get install vim 
-
+#!
+cd ~/Public
+sudo apt-get install git
+git config --global user.email "mick.sexton@gmail.com"
+git config --global user.name "Michael Sexton"
+git clone https://github.com/comtesacristain/linux-setup.git
+cd linux-setup
 
 ./apps.sh
 ./ruby.sh
+
+sudo apt-get upgrade
+
+
