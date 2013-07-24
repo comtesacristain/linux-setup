445f25f84caca220d68ff9404569552b75d29f31
Michael Sexton <mick.sexton@gmail.com>
2013-07-24 22:06:14 +1000

    Removed duplicate files now in the scripts fodler

--- oracle.sh
+++
@@ -1,7 +0,0 @@
-#!
-echo "deb http://oss.oracle.com/debian unstable main non-free" | sudo tee -a /etc/apt/sources.list
-wget http://oss.oracle.com/el4/RPM-GPG-KEY-oracle  -O- | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install oracle-xe-universal
-sudo /etc/init.d/oracle-xe configure
-cat oracle/.oraclerc >> ~/.bashrc
--- ruby.sh
+++
@@ -1,3 +0,0 @@
-#!
-\curl -L https://get.rvm.io | bash -s stable --ruby
-# TODO: Add something that installs rvm to profile
