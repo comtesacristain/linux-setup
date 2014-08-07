7a1064f9d3f7a8ee5806b96387f21e8cad1c467a
Michael Sexton <mick.sexton@gmail.com>
2014-08-07 13:51:15 +1000

    This is to help install Oracle 11g on Ubuntu

---
+++ oracle/links.sh
@@ -0,0 +1,2 @@
+# http://meandmyubuntulinux.blogspot.ca/2012/05/installing-oracle-11g-r2-express.html
+# http://stackoverflow.com/questions/16545412/problems-installing-oracle-database-express-edition-11g
